b411cade758fbcd934cffaf3f174f6d7a6856aff
Andrew Ostapov <ost.and.g@gmail.com>
2020-04-05 23:22:57 +0300

    New components: 'wallet-dashboard', 'wallet-ID'. Route Parameters.

---
+++ src/app/wallet/wallet-id/wallet-id.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-id',
+  templateUrl: './wallet-id.component.html',
+  styleUrls: ['./wallet-id.component.scss']
+})
+export class WalletIDComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
