1d198afa660a528f7658f3e716bdb935bd1ec4e6
Andrew Ostapov <ost.and.g@gmail.com>
2020-05-12 00:13:53 +0300

    First communication with front and back ends. Added user with wallets to front with authentification + logic

--- src/app/wallet/services/wallet-resource.ts
+++ src/app/wallet/services/wallet-resource.ts
@@ -6,7 +6,7 @@ import {IWalletListItem} from './dataWallet/wallet-list-item';
 
 @Injectable()
 export class WalletResource {
-  private readonly URL = ApiConfig.url + '/dashboard';
+  private readonly URL = ApiConfig.url + '/wallets';
   constructor(private httpClient: HttpClient) {
   }
   public findAll(): Observable<IWalletListItem[]> {
