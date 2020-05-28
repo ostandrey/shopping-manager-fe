8914401429b59cc08f403efb6fa10081352ef0ed
Andrew Ostapov <ost.and.g@gmail.com>
2020-05-28 21:03:47 +0300

    Add media queries, modify styles, add overview

--- src/app/wallet/wallet-overview/wallet-overview.component.ts
+++ src/app/wallet/wallet-overview/wallet-overview.component.ts
@@ -1,40 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {FlatTreeControl, NestedTreeControl} from '@angular/cdk/tree';
-import {MatTreeFlatDataSource, MatTreeFlattener, MatTreeNestedDataSource} from '@angular/material/tree';
 
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
-}
-
-const TREE_DATA: FoodNode[] = [
-  {
-    name: 'Fruit',
-    children: [
-      {name: 'Apple'},
-      {name: 'Banana'},
-      {name: 'Fruit loops'},
-    ]
-  }, {
-    name: 'Vegetables',
-    children: [
-      {
-        name: 'Green',
-        children: [
-          {name: 'Broccoli'},
-          {name: 'Brussels sprouts'},
-        ]
-      }, {
-        name: 'Orange',
-        children: [
-          {name: 'Pumpkins'},
-          {name: 'Carrots'},
-        ]
-      },
-    ]
-  },
-];
 
 
 
@@ -44,13 +9,5 @@ const TREE_DATA: FoodNode[] = [
   styleUrls: ['./wallet-overview.component.scss']
 })
 export class WalletOverviewComponent {
-  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
-  dataSource = new MatTreeNestedDataSource<FoodNode>();
-
-  constructor() {
-    this.dataSource.data = TREE_DATA;
-  }
-
-  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
 
 }
